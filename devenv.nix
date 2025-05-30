cabe24a3084e0b159723f25f1c81386a59a77bd5
leiserfg <leiserfg@gmail.com>
2025-05-30 23:26:25 +0200

    Base

--- devenv.nix
+++ devenv.nix
@@ -9,10 +9,15 @@
 {
   packages = [
     pkgs.tinymist
+    pkgs.pympress
   ];
 
   languages.typst = {
     enable = true;
+    fontPaths = [
+      "${(pkgs.iosevka-bin.override { variant = "SGr-IosevkaSS15"; })}/share/fonts/truetype"
+      "${pkgs.noto-fonts-emoji}/share/fonts/noto"
+    ];
   };
 
 }
